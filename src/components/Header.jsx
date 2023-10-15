a07a51fe5407f8a4e1b8cb390df6533452e30418
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-16 01:29:02 +0600

    Search Field Implemented On Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
+import { IoMdCart } from 'react-icons/io'
 
 const Header = () => {
 	return (
 		<div className="bg-bodyColor h-20">
 			<Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
 				<Logo />
+				{/* <===<<=== Search Field ===>>===> */}
+				<div>
+					<input
+						type="text"
+						placeholder="Search for products"
+						className="placeholder:text-sm flex-1 outline-none"
+					/>
+				</div>
+				{/* <===<<=== Login/Register ===>>===> */}
+				{/* <===<<=== Cart Button ===>>===> */}
 			</Container>
 		</div>
 	);
