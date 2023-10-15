30a1cfc03c28b7bf39476ea7f1567e6a539cbbe1
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-15 23:06:24 +0600

    Body Font Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque itaque eius sapiente nihil libero hic nesciunt dolorem non eaque sint.
-    </div>
-  )
-}
+	return (
+		<div>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
+			itaque eius sapiente nihil libero hic nesciunt dolorem non eaque
+			sint.
+		</div>
+	);
+};
 
-export default Home
+export default Home;
