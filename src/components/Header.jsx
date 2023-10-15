3318c21dfd654ec19ff7c66867d16f3e08761146
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-16 01:41:28 +0600

    Search Field Nodify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
 			<Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
 				<Logo />
 				{/* <===<<=== Search Field ===>>===> */}
-				<div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600">
-					<FiSearch />
+				<div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600 group">
+					<FiSearch className="text-gray-500 group-focus-within:text-darkText transition-all duration-200" />
 					<input
 						type="text"
 						placeholder="Search for products"
