3209a88cce7305d0ad01765ded36094255d6b2a0
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-16 12:29:18 +0600

    Cart Button Implemented On Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,8 @@ const Header = () => {
 				{/* <===<<=== Cart Button ===>>===> */}
 				<div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 cursor-pointer border border-black hover:border-orange-600 transition-all duration-200 relative">
 					<IoMdCart className="text-2xl" />
-					<p className="text-sm font-semibold">$0.00</p>
+					<p className="">$0.00</p>
+					<span className="bg-white text-orange-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">0</span>
 				</div>
 			</Container>
 		</div>
