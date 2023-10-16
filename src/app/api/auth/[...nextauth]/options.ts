145ba5bc2446bb672dd173df4a4f34c3d7fe51ef
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-16 15:14:07 +0600

    Google Credentials Implemented

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,11 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const options:  NextAuthOptions = {
-   providers: [
-      GithubProvider({
-         clientId: process.env.GITHUB_ID!,
-         clientSecret: process.env.GITHUB_SECRET!,
-      })
-   ]
-}
+export const options: NextAuthOptions = {
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID!,
+			clientSecret: process.env.GITHUB_SECRET!,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+};
