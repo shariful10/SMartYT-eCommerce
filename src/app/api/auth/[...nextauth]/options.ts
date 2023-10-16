5dedccacc7f1d23c92954db3fe150d1aab9459e8
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-16 15:27:26 +0600

    Implement Credentials & Authorize

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -13,5 +13,35 @@ export const options: NextAuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: {
+					label: "Username",
+					type: "text",
+					placeholder: "Enter your username",
+				},
+				password: {
+					label: "Password",
+					type: "password",
+					placeholder: "Enter your password",
+				},
+			},
+			async authorize(credentials) {
+				const user = {
+					id: "1001",
+					name: "Shariful",
+					password: "sk112233",
+				};
+				if (
+					credentials?.username === user.name &&
+					credentials?.password === user.password
+				) {
+					return user;
+				} else {
+					return null;
+				}
+			},
+		}),
 	],
 };
