592c51e962f963ebc5e88bead8dc784f63f5ffe0
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-17 20:15:14 +0600

    Image Implemented For Product Card

--- src/components/ProductsData.tsx
+++ src/components/ProductsData.tsx
@@ -1,13 +1,24 @@
-"use client"
+"use client";
 
+import Image from "next/image";
 import { ItemProps } from "../../types";
 
-const ProductsData = ({item}: ItemProps) => {
-  console.log(item, 'Product');
+const ProductsData = ({ item }: ItemProps) => {
+	console.log(item, "Product");
 
-  return (
-    <div>ProductsData</div>
-  )
-}
+	return (
+		<div className="w-full rounded-lg overflow-hidden">
+			<div className="w-full h-96 group overflow-hidden relative">
+				<Image
+					src={item?.image}
+					width={500}
+					height={500}
+					alt="Product Image"
+					className="w-full h-full"
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default ProductsData
+export default ProductsData;
