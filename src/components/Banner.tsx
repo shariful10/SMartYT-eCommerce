89f061308eb17e9b68c8eb6a411226f440dc9d5a
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-17 15:03:30 +0600

    Completed The Implementation Banner Slider

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,8 @@ import Slider from "react-slick";
 import bannerone from "@/images/bannerone.jpg";
 import bannertwo from "@/images/bannertwo.jpg";
 import bannerthree from "@/images/bannerthree.jpg";
-import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
+// import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import BannerText from "./BannerText";
 
 const Banner = () => {
@@ -13,10 +14,10 @@ const Banner = () => {
 		const { onClick } = props;
 		return (
 			<div
-				className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer transition-all duration-200 rounded-full text-2xl flex items-center z-20 absolute left-5 top-1/2"
+				className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer transition-all duration-200 rounded-full text-2xl flex items-center z-20 absolute left-5 top-[35%] md:top-[40%] lg:top-1/2"
 				onClick={onClick}
 			>
-				<PiCaretLeftLight />
+				<FaAngleLeft />
 			</div>
 		);
 	};
@@ -25,10 +26,10 @@ const Banner = () => {
 		const { onClick } = props;
 		return (
 			<div
-				className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer transition-all duration-200 rounded-full text-2xl flex items-center z-20 absolute right-5 top-1/2"
+				className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer transition-all duration-200 rounded-full text-2xl flex items-center z-20 absolute right-5 top-[35%] md:top-[40%] lg:top-1/2"
 				onClick={onClick}
 			>
-				<PiCaretRightLight />
+				<FaAngleRight />
 			</div>
 		);
 	};
@@ -40,8 +41,8 @@ const Banner = () => {
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		nextArrow: <NextArrow />,
-		prevArrow: <PrevArrow />,
+		nextArrow: <PrevArrow />,
+		prevArrow: <NextArrow />,
 	};
 
 	return (
