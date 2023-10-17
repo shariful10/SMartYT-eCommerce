4d19f870de569b0afd5c11c382b8acfce81f19b3
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-17 15:54:31 +0600

    Get All Products Data

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { productData } from "@/constants/data";
+
+export const GET = async () => {
+	try {
+        return NextResponse.json({
+            message: "Product fetch successfully",
+            success: true,
+            productData,
+        })
+	} catch (error) {
+		return NextResponse.json(
+			{
+				error: "Product data not found",
+			},
+			{ status: 500 }
+		);
+	}
+};
