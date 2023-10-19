605fe7d888b7e8d5074562feaff5e04bf058b800
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-20 03:08:37 +0600

    Price Implemented On Product Table

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "next/image";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Products, StateProps } from "../../types";
 import { AiOutlineClose } from "react-icons/ai";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { decreaseQuantity, increaseQuantity } from "@/redux/shoppingSlice";
+import FormattedPrice from "./FormattedPrice";
 
 const CartItem = () => {
 	const { productData } = useSelector((state: StateProps) => state?.shopping);
+	const dispatch = useDispatch();
 
 	return (
-		<div className="flex flex-col gap-y-2">
+		<div className="flex flex-col gap-y-2 min-h-[calc(100vh-380px)]">
 			<div className="hidden lg:inline-flex items-center justify-between font-semibold bg-white p-2 rounded-[4px]">
 				<p className="w-1/3">Product</p>
 				<p className="w-1/3 flex items-center justify-center">
@@ -18,43 +21,65 @@ const CartItem = () => {
 				<p className="w-1/3 flex items-center justify-end">Subtotal</p>
 			</div>
 			{/* <===<<=== Generate The Product ===>>===> */}
-			<div className="flex flex-col gap-y-2">
-				{productData?.map((item: Products) => (
-					<div
-						key={item._id}
-						className="w-full bg-white p-4 flex flex-col md:flex-row rounded-[4px] items-center justify-between gap-4"
-					>
-						<div className="flex items-center gap-x-3 w-full md:w-1/3">
-							<span className="text-lg hover:text-red-600 cursor-pointer duration-200">
-								<AiOutlineClose />
-							</span>
-							<Image
-								src={item?.image}
-								width={500}
-								height={500}
-								alt="Product Image"
-								className="w-20 h-20 object-cover rounded-[4px]"
-							/>
-						</div>
-						{/* <===<<=== Quantity ===>>===> */}
-						<div className="flex items-center justify-start gap-x-3 border border-slate-300 py-2 px-4 w-full md:w-auto">
-							<p>Quantity</p>
-							<div className="flex items-center text-lg w-20 justify-between">
-								<span className="cursor-pointer">
-									<FiChevronLeft />
+			{productData.length !== 0 ? (
+				<div className="flex flex-col gap-y-2">
+					{productData?.map((item: Products) => (
+						<div
+							key={item._id}
+							className="w-full bg-white p-4 flex flex-col md:flex-row rounded-[4px] items-center justify-between gap-4"
+						>
+							<div className="flex items-center gap-x-3 w-full md:w-1/3">
+								<span className="text-lg hover:text-red-600 cursor-pointer duration-200">
+									<AiOutlineClose />
 								</span>
-								<span>{item?.quantity}</span>
-								<span className="cursor-pointer">
-									<FiChevronRight />
-								</span>
+								<Image
+									src={item?.image}
+									width={500}
+									height={500}
+									alt="Product Image"
+									className="w-20 h-20 object-cover rounded-[4px]"
+								/>
+							</div>
+							{/* <===<<=== Quantity ===>>===> */}
+							<div className="flex items-center justify-start gap-x-3 border border-slate-300 py-2 px-4 w-full md:w-auto">
+								<p>Quantity</p>
+								<div className="flex items-center text-lg w-20 justify-between">
+									<span
+										onClick={() =>
+											dispatch(decreaseQuantity(item))
+										}
+										className="cursor-pointer"
+									>
+										<FiChevronLeft />
+									</span>
+									<span>{item?.quantity}</span>
+									<span
+										onClick={() =>
+											dispatch(increaseQuantity(item))
+										}
+										className="cursor-pointer"
+									>
+										<FiChevronRight />
+									</span>
+								</div>
+							</div>
+							<div className="w-full md:w-1/3 flex items-end justify-start md:justify-end">
+								<p className="text-lg font-semibold">
+									<FormattedPrice
+										amount={item?.price * item?.quantity}
+									/>
+								</p>
 							</div>
 						</div>
-						<div>
-							<p>Price</p>
-						</div>
-					</div>
-				))}
-			</div>
+					))}
+				</div>
+			) : (
+				<div className="flex flex-col justify-center items-center">
+					<p className="text-3xl font-semibold mt-5">
+						You have not selected any product yet!
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
