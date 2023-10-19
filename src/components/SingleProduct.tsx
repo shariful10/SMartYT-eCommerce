475118eb333bc0551620ebac558ff116e935ae67
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-19 20:39:50 +0600

    Product Title Implemented On Single Product Page

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -13,9 +13,14 @@ const SingleProduct = ({ product }: any) => {
 				height={500}
 				width={500}
 				alt="Product image"
-        className="w-full max-h-[700px] object-cover rounded-lg"
+        className="w-full max-h-[500px] object-cover rounded-lg"
 			/>
 		</div>
+    <div className="">
+      <div className="">
+        <p className="">{product?.title}</p>
+      </div>
+    </div>
     </div>
 	);
 };
