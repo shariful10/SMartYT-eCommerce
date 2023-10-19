517938c507f1dd26fe4bb777b89b450ad73bc69b
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-20 02:16:54 +0600

    Table Head Implemented On Cart Page

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { StateProps } from '../../types';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { StateProps } from "../../types";
+import { useSelector } from "react-redux";
 
 const CartItem = () => {
-    const { productData } = useSelector((state: StateProps) => state?.shopping);
+	const { productData } = useSelector((state: StateProps) => state?.shopping);
 
-  return (
-    <div>CartItem</div>
-  )
-}
+	return (
+		<div className="flex flex-col gap-y-2">
+			<div className="hidden lg:inline-flex items-center justify-between font-semibold bg-white p-2">
+				<p className="w-1/3">Product</p>
+				<p className="w-1/3 flex items-center justify-center">Quantity</p>
+				<p className="w-1/3 flex items-center justify-end">Subtotal</p>
+			</div>
+		</div>
+	);
+};
 
-export default CartItem
+export default CartItem;
