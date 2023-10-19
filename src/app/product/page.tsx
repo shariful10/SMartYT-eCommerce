c3cc8ae2c5c75c06e97381d4eddd7ba94f3a8c8c
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-19 20:37:29 +0600

    Product Image Implemented On Single Product Page

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container";
 import { getSingleProduct, getTrendingProducts } from "@/helpers";
 import { Products } from "../../../types";
 import ProductsData from "@/components/ProductsData";
+import SingleProduct from "@/components/SingleProduct";
 
 type Props = {
 	searchParams: { [key: string]: string | string[] | undefined };
@@ -15,8 +16,9 @@ const ProductPage = async ({ searchParams }: Props) => {
 
 	return (
 		<Container>
+      <SingleProduct product={product} />
 			<div className="">
-				<p className="">Tranding Products</p>
+				<p className="text-xl py-1 font-semibold">Tranding Products</p>
 				<div className="grid md:grid-cols-2 xl:grid-cols-4 gap-4">
 					{data?.map((item: Products) => (
 						<ProductsData key={item._id} item={item} />
---
+++ src/components/SingleProduct.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+
+const SingleProduct = ({ product }: any) => {
+	console.log(product);
+
+	return (
+		<div className="grid lg:grid-cols-2 gap-5 bg-white p-4 rounded-lg">
+      <div>
+			<Image
+				src={product?.image}
+				height={500}
+				width={500}
+				alt="Product image"
+        className="w-full max-h-[700px] object-cover rounded-lg"
+			/>
+		</div>
+    </div>
+	);
+};
+
+export default SingleProduct;
