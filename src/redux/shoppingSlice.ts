c038744aef9e64a24452f1fa17f23cd56b251b1b
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-20 00:24:25 +0600

    Updated

--- src/redux/shoppingSlice.ts
+++ src/redux/shoppingSlice.ts
@@ -2,32 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 import { Products } from "../../types";
 
 interface StoreState {
-    productData: Products[];
-    userInfo: null | string;
-    orderData: [];
+	productData: Products[];
+	userInfo: null | string;
+	orderData: [];
 }
 
 const initialState: StoreState = {
-    productData: [],
-    userInfo: null,
-    orderData: [],
+	productData: [],
+	userInfo: null,
+	orderData: [],
 };
 
 export const shoppingSlice = createSlice({
-    name: 'shopping',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            const existingProduct = state.productData.find((item: Products) => item._id === action.payload._id);
+	name: "shopping",
+	initialState,
+	reducers: {
+		addToCart: (state, action) => {
+			const existingProduct = state.productData.find(
+				(item: Products) => item._id === action.payload._id
+			);
 
-            if(existingProduct) {
-                existingProduct.quantity += action.payload.quantity
-            } else {
-                state.productData.push(action.payload);
-            }
-        }
-    }
+			if (existingProduct) {
+				existingProduct.quantity += action.payload.quantity;
+			} else {
+				state.productData.push(action.payload);
+			}
+		},
+	},
 });
 
-export const {addToCart} = shoppingSlice.actions;
+export const { addToCart } = shoppingSlice.actions;
 export default shoppingSlice.reducer;
