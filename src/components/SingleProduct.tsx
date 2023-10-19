e84939923456eaa28162df43eb7645460eac17f4
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-19 20:44:00 +0600

    Product Price Styled Implemented On Single Product Page

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -8,22 +8,24 @@ const SingleProduct = ({ product }: any) => {
 
 	return (
 		<div className="grid lg:grid-cols-2 gap-5 bg-white p-4 rounded-lg">
-      <div>
-			<Image
-				src={product?.image}
-				height={500}
-				width={500}
-				alt="Product image"
-        className="w-full max-h-[500px] object-cover rounded-lg"
-			/>
+			<div>
+				<Image
+					src={product?.image}
+					height={500}
+					width={500}
+					alt="Product image"
+					className="w-full max-h-[500px] object-cover rounded-lg"
+				/>
+			</div>
+			<div className="">
+				<div className="">
+					<p className="text-3xl font-semibold">{product?.title}</p>
+					<p className="text-xl font-semibold">
+						<FormattedPrice amount={product?.price} />
+					</p>
+				</div>
+			</div>
 		</div>
-    <div className="">
-      <div className="">
-        <p className="text-3xl font-semibold">{product?.title}</p>
-        <p className=""><FormattedPrice amount={product?.price} /></p>
-      </div>
-    </div>
-    </div>
 	);
 };
 
