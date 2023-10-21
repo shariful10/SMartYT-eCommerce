4bc8ae8e54a20cdc561c91f74fbb933e1c2e0ce8
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-21 17:18:58 +0600

    Reimplemented Everything

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -2,14 +2,14 @@ import Container from "@/components/Container";
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const Success = () => {
 	return (
 		<Container className="flex items-center justify-center min-h-[calc(100vh-380px)]">
 			<div className="flex flex-col items-center justify-center gap-y-5">
-				<h2 className="text-4xl font-bold">
+				<h2 className="text-4xl font-bold text-green-600">
 					Your Payment Accepted By shoppingmart.varcel.app 🎉
 				</h2>
-				<p className="">
+				<p className="text-xl">
 					Now you can view your oders or continue shopping with us
 				</p>
 				<div className="flex items-center gap-x-5">
@@ -29,4 +29,4 @@ const page = () => {
 	);
 };
 
-export default page;
+export default Success;
